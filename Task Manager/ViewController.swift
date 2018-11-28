61cfbe26db10ba318e329820f2b556817d9be85a
Kaleb Spillman <kalebspillman@Interapts-MacBook-Pro-5.local>
2018-11-28 08:09:39 -0600

    Initial Commit

---
+++ Task Manager/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Task Manager
+//
+//  Created by Kaleb Spillman on 11/28/18.
+//  Copyright © 2018 Kaleb Spillman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
